46d0339629d7a55ee03a2685c265f8c3b8c2301e
ambrosiomaxwell <63318679+ambrosiomaxwell@users.noreply.github.com>
2021-01-05 18:20:05 -0500

    final push
    pushing final version

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# WorkDayPlanner
-A daily planner to Plan out events for the work day.
+Welcome to the Daily planner!
+
+Are you tired of losing track of all the responsibilities you have throughout the day?
+Are you looking for a way to get your life back on track?
+
+Well then the Daily Planner app is just for you!
+
+Using advanced web technologies you can plan out your work day exactly how it needs to go.
+
+Continue reading for further information on this service...
+
+
+
+
+One thing to note:
+- Colors of the block show what time of day it is
+    Red = past
+    Blue = present
+    Green = future
+
+
+HOW TO USE:
+
+1.) Enter the task you need completed at the time you want to do the task
+
+2.) Click the save button
+
+3.) That task will be saved. Feel free to close out and come back later!
---
+++ script.js
@@ -0,0 +1,105 @@
+//lays out times for the day
+//
+times = [
+    { displayTime: '9:00AM',
+    check:'9',
+     tasks:''},
+     { displayTime: '10:00AM',
+     check:'10',
+     tasks:''},
+     { displayTime: '11:00AM',
+     check:'11',
+     tasks:''},
+     { displayTime: '12:00PM',
+     check:'12',
+     tasks:''},
+     { displayTime: '1:00PM',
+     check:'13',
+     tasks:''},
+     { displayTime: '2:00PM',
+     check:'14',
+     tasks:''},
+     { displayTime: '3:00PM',
+     check:'15',
+     tasks:''},
+     { displayTime: '4:00PM',
+     check:'16',
+     tasks:''},
+     { displayTime: '5:00PM',
+     check:'17',
+     tasks:''},
+
+]
+
+//retieves saved string and formats it back into an array
+retrieve = localStorage.getItem("tasks")
+times = JSON.parse(retrieve)
+//sets the current day on the jumbotron
+date = dayjs().format('dddd[, ]MMMM[ ]DD[, ]YYYY')
+
+$('#displayDate').text(date)
+
+//saves the hour of the current day
+hour = dayjs().hour()
+
+times.forEach(function(item){
+    //creates the row for the hour
+    row = $('<div>').addClass('shadow-lg row row-class rounded')
+
+    //creates the column for the time
+    //grabs the time for this row
+    timeCol=$('<div>').addClass('col-md-2 blockquote').text(item.displayTime)
+    row.append(timeCol)
+
+    //creates task box
+    taskCol = $('<textarea>').addClass('col-md-9 boards task rounded').attr('rows', '4').val(item.tasks)
+    shownTime = item.displayTime[0] + item.displayTime[1]
+
+    //checks too see if time before or after current blcok. changes color accordingly
+    if(hour == item.check){
+        taskCol.addClass('present')
+        }
+    else if(hour < item.check){
+        taskCol.addClass('future')
+    }
+    else if(hour > item.check){
+        taskCol.addClass('past')
+    }
+
+    //creates button column and the button
+    buttonCol = $('<div>').addClass('col-md-1 boards rounded')
+    button = $('<button>').addClass('btn-primary btn-lg btn-block listening').attr('type', 'button')
+    button.append('<i class="fas fa-save"></i>')
+    buttonCol.append(button)
+    
+    //final row setup and append
+    row.attr('id', item.displayTime)
+    row.append(taskCol)
+    row.append(buttonCol)
+    //adds the whole row to the container div
+    $('#holder').append(row)
+    //listens for button click
+    $('.listening').click(save)
+});
+
+function save(e){
+    e.stopPropagation()
+    console.log('hi')
+     //grabs the id of the row and the text entered in the text area
+    let verify = $(this).parent().parent().attr('id')
+    let newTask = $(this).parent().siblings('.task').val()
+//sets the row task in the array and saves it to local storage
+    times.forEach(function(item){
+        if(item.displayTime == verify){
+
+            item.tasks = newTask
+            localStorage.setItem('tasks', JSON.stringify(times))
+            retrieve2 = localStorage.getItem("tasks")
+            times = JSON.parse(retrieve2)
+
+        }
+    })
+    
+}
+
+
